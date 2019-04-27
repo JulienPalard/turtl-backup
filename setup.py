b92ec53aef3d0b2a2a8be5201c2929d432b00b46
Julien Palard <julien@palard.fr>
2019-04-27 19:35:13 +0200

    Missing long description content type.

--- setup.py
+++ setup.py
@@ -2,39 +2,37 @@
 
 from setuptools import setup
 
-with open('README.md') as readme_file:
+with open("README.md") as readme_file:
     readme = readme_file.read()
 
 setup(
-    name='turtl-backup',
-    version='0.1.0',
+    name="turtl-backup",
+    version="0.1.0",
     description="Tool to backup a turtl account.",
     long_description=readme,
+    long_description_content_type="text/markdown",
     author="Julien Palard",
-    author_email='julien@palard.fr',
-    url='https://github.com/JulienPalard/turtle-backup',
+    author_email="julien@palard.fr",
+    url="https://github.com/JulienPalard/turtle-backup",
     entry_points={
-        'console_scripts': [
-            'turtl-backup=turtl_backup.turtl_backup:main',
-            'turtl-backup-gui=turtl_backup.gui:main'
+        "console_scripts": [
+            "turtl-backup=turtl_backup.turtl_backup:main",
+            "turtl-backup-gui=turtl_backup.gui:main",
         ]
     },
-    packages=['turtl_backup'],
-    install_requires=[
-        'requests',
-        'cryptography'
-    ],
+    packages=["turtl_backup"],
+    install_requires=["requests", "cryptography"],
     license="MIT",
-    keywords='turtl backup',
+    keywords="turtl backup",
     classifiers=[
-        'Development Status :: 2 - Pre-Alpha',
-        'Intended Audience :: Developers',
-        'License :: OSI Approved :: MIT License',
-        'Natural Language :: English',
-        'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.3',
-        'Programming Language :: Python :: 3.4',
-        'Programming Language :: Python :: 3.5',
-        'Programming Language :: Python :: 3.6',
+        "Development Status :: 2 - Pre-Alpha",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: MIT License",
+        "Natural Language :: English",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.3",
+        "Programming Language :: Python :: 3.4",
+        "Programming Language :: Python :: 3.5",
+        "Programming Language :: Python :: 3.6",
     ],
 )
